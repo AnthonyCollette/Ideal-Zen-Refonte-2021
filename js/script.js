fa7460ae974c3c8e3884bad9a2d9bb3baf886f78
Anthony <100pour100inde@gmail.com>
2021-04-19 14:21:14 +0200

    bouton retour vers le haut

--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@ const burger = document.getElementById("btn-responsive");
 const nav = document.getElementById("nav");
 const navul = document.getElementById("nav-ul");
 const article = document.getElementById("article-li");
+const upBtn = document.getElementsByClassName("up");
+const header = document.querySelector("header");
+
+function backToUp() {
+  header.scrollIntoView({ behavior: "smooth", block: "start" });
+}
+
+for (i = 0; i < upBtn.length; i++) {
+  upBtn[i].addEventListener("click", backToUp);
+}
 
 burger.addEventListener("click", () => {
   nav.classList.toggle("show");
